f5282f7e174fd75b1b1a6e2a503b3b20185591e6
asfandyar97 <asfandyarkha97>
2025-02-20 17:32:37 +0500

    ghgj

--- src/pages/Placeorder/Placeorder.jsx
+++ src/pages/Placeorder/Placeorder.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import './placeorder.css'
+import '/placeorder.css'
 import { StoreContext } from '../../context/StoreContext'
 const placeorder = () => {
  
